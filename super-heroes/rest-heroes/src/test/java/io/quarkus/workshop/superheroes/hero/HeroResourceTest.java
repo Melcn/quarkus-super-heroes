7c2e97a22569370d3e1d1279fba1bacc0e352ee8
Melcn <melaniearinosy@outlook.com>
2023-09-15 14:05:15 +0200

    create addinvalidItems

--- super-heroes/rest-heroes/src/test/java/io/quarkus/workshop/superheroes/hero/HeroResourceTest.java
+++ super-heroes/rest-heroes/src/test/java/io/quarkus/workshop/superheroes/hero/HeroResourceTest.java
@@ -61,4 +61,33 @@ public class HeroResourceTest {
                 .statusCode(OK.getStatusCode())
                 .contentType(APPLICATION_JSON);
     }
+
+    @Test
+    void shouldNotAddInvalidItem() {
+        Hero hero = new Hero();
+        hero.name = null;
+        hero.otherName = DEFAULT_OTHER_NAME;
+        hero.picture = DEFAULT_PICTURE;
+        hero.powers = DEFAULT_POWERS;
+        hero.level = 0;
+
+        given()
+                .body(hero)
+                .header(CONTENT_TYPE, APPLICATION_JSON)
+                .header(ACCEPT, APPLICATION_JSON)
+                .when()
+                .post("/api/heroes")
+                .then()
+                .statusCode(BAD_REQUEST.getStatusCode());
+    }
+
+    @Test
+    @Order(1)
+    void shouldGetInitialItems() {
+        List<Hero> heroes = get("/api/heroes").then()
+                .statusCode(OK.getStatusCode())
+                .contentType(APPLICATION_JSON)
+                .extract().body().as(getHeroTypeRef());
+        assertEquals(NB_HEROES, heroes.size());
+    }
 }
