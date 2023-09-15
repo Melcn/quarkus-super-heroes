76063e85c210ed5af484363dda26a7d7a7ac3cf4
Melcn <melaniearinosy@outlook.com>
2023-09-15 11:49:12 +0200

    create + init get randomhero

--- super-heroes/rest-heroes/src/main/java/io/quarkus/workshop/superheroes/hero/HeroResource.java
+++ super-heroes/rest-heroes/src/main/java/io/quarkus/workshop/superheroes/hero/HeroResource.java
@@ -6,7 +6,38 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
 @Path("/api/heroes")
+@Tag(name = "heroes")
 public class HeroResource {
+
+    Logger logger;
+
+    public HeroResource(Logger logger) {
+        this.logger = logger;
+    }
+
+    @Operation(summary = "Returns a random hero")
+    @GET
+    @Path("/random")
+    @APIResponse(responseCode = "200", content = @Content(mediaType = APPLICATION_JSON, schema = @Schema(implementation = Hero.class, required = true)))
+    public Uni<RestResponse<Hero>> getRandomHero() {
+        return Hero.findRandom()
+                .onItem().ifNotNull().transform(h -> {
+                    this.logger.debugf("Found random hero: %s", h);
+                    return RestResponse.ok(h);
+                })
+                .onItem().ifNull().continueWith(() -> {
+                    this.logger.debug("No random villain found");
+                    return RestResponse.notFound();
+                });
+    }
+
+    @Operation(summary = "Returns all the heroes from the database")
+    @GET
+    @APIResponse(responseCode = "200", content = @Content(mediaType = APPLICATION_JSON, schema = @Schema(implementation = Hero.class, type = SchemaType.ARRAY)))
+    public Uni<List<Hero>> getAllHeroes() {
+        return Hero.listAll();
+    }
+
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
