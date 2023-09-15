595e1a95da2d0378e98786c7bdb18a3e30ab96ab
Melcn <melaniearinosy@outlook.com>
2023-09-15 16:05:30 +0200

    init FightresourceTest in rest fight service

--- super-heroes/rest-fights/src/test/java/io/quarkus/workshop/superheroes/fight/FightResourceTest.java
+++ super-heroes/rest-fights/src/test/java/io/quarkus/workshop/superheroes/fight/FightResourceTest.java
@@ -1,13 +1,44 @@
 package io.quarkus.workshop.superheroes.fight;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.workshop.superheroes.fight.client.Hero;
+import io.quarkus.workshop.superheroes.fight.client.Villain;
+import io.restassured.common.mapper.TypeRef;
+import org.hamcrest.core.Is;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
+import java.util.List;
+
+import java.util.Random;
+import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
+import static jakarta.ws.rs.core.HttpHeaders.ACCEPT;
+import static jakarta.ws.rs.core.HttpHeaders.CONTENT_TYPE;
+import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
+import static jakarta.ws.rs.core.Response.Status.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @QuarkusTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class FightResourceTest {
+
+
+    private static final String DEFAULT_WINNER_NAME = "Super Baguette";
+    private static final String DEFAULT_WINNER_PICTURE = "super_baguette.png";
+    private static final int DEFAULT_WINNER_LEVEL = 42;
+    private static final String DEFAULT_WINNER_POWERS = "Eats baguette in less than a second";
+    private static final String DEFAULT_LOSER_NAME = "Super Chocolatine";
+    private static final String DEFAULT_LOSER_PICTURE = "super_chocolatine.png";
+    private static final int DEFAULT_LOSER_LEVEL = 6;
+    private static final String DEFAULT_LOSER_POWERS = "Transforms chocolatine into pain au chocolat";
+
+    private static final int NB_FIGHTS = 3;
+    private static String fightId;
 
     @Test
     public void testHelloEndpoint() {
