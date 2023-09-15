947d9363c075a736fcfa651c0f75cfa68b3a2a2c
Melcn <melaniearinosy@outlook.com>
2023-09-15 15:59:57 +0200

    create getRadomFight in FightResource

--- super-heroes/rest-fights/src/main/java/io/quarkus/workshop/superheroes/fight/FightResource.java
+++ super-heroes/rest-fights/src/main/java/io/quarkus/workshop/superheroes/fight/FightResource.java
@@ -1,12 +1,36 @@
 package io.quarkus.workshop.superheroes.fight;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import org.jboss.logging.Logger;
+
+import jakarta.inject.Inject;
+import jakarta.validation.Valid;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.UriInfo;
+
+import java.util.List;
+
+import static jakarta.ws.rs.core.MediaType.*;
 
 @Path("/api/fights")
+@Produces(APPLICATION_JSON)
 public class FightResource {
+
+    @Inject Logger logger;
+
+    @Inject
+    FightService service;
+
+    @GET
+    @Path("/randomfighters")
+    public Response getRandomFighters() {
+        Fighters fighters = service.findRandomFighters();
+        logger.debug("Get random fighters " + fighters);
+        return Response.ok(fighters).build();
+    }
+
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
