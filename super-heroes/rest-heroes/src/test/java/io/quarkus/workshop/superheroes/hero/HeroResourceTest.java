972c61d570282dcb2704de0c594943295782d37e
Melcn <melaniearinosy@outlook.com>
2023-09-15 14:02:29 +0200

    init HeroresourcesTest

--- super-heroes/rest-heroes/src/test/java/io/quarkus/workshop/superheroes/hero/HeroResourceTest.java
+++ super-heroes/rest-heroes/src/test/java/io/quarkus/workshop/superheroes/hero/HeroResourceTest.java
@@ -7,7 +7,24 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class HeroResourceTest {
+
+    private static final String JSON = "application/json;charset=UTF-8";
+
+    private static final String DEFAULT_NAME = "Super Baguette";
+    private static final String UPDATED_NAME = "Super Baguette (updated)";
+    private static final String DEFAULT_OTHER_NAME = "Super Baguette Tradition";
+    private static final String UPDATED_OTHER_NAME = "Super Baguette Tradition (updated)";
+    private static final String DEFAULT_PICTURE = "super_baguette.png";
+    private static final String UPDATED_PICTURE = "super_baguette_updated.png";
+    private static final String DEFAULT_POWERS = "eats baguette really quickly";
+    private static final String UPDATED_POWERS = "eats baguette really quickly (updated)";
+    private static final int DEFAULT_LEVEL = 42;
+    private static final int UPDATED_LEVEL = 43;
+
+    private static final int NB_HEROES = 941;
+    private static String heroId;
 
     @Test
     public void testHelloEndpoint() {
