f597a64178d644dc8ee495ad37553a6328b445ad
Melcn <melaniearinosy@outlook.com>
2023-10-06 15:11:37 +0200

    feat(RESTFights): create FinrandomHero in FightService

--- super-heroes/rest-fights/src/main/java/io/quarkus/workshop/superheroes/fight/FightService.java
+++ super-heroes/rest-fights/src/main/java/io/quarkus/workshop/superheroes/fight/FightService.java
@@ -105,4 +105,8 @@ public class FightService {
         return fight;
     }
 
+    @Fallback(fallbackMethod = "fallbackRandomHero")
+    Hero findRandomHero() {
+        return heroProxy.findRandomHero();
+    }
 }
